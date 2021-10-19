ed58069f9b0e6e10f924965f63b50a9a0618d428
Jakub Sokołowski <jakub@status.im>
2021-10-19 22:57:07 +0200

    add outputs: public_ips, hostnames, hosts, ids
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,20 @@
+locals {
+  public_ips   = [for h in hcloud_floating_ip.host : h.ip_address]
+  instance_ids = [for h in hcloud_server.host : h.id]
+}
+
+output "public_ips" {
+  value = local.public_ips
+}
+
+output "hostnames" {
+  value = local.hostnames
+}
+
 output "hosts" {
-  value = {
-    for instance in hcloud_server.host :
-    instance.name => hcloud_floating_ip.host[instance.name].ip_address
-  }
+  value = zipmap(local.hostnames, local.public_ips)
+}
+
+output "ids" {
+  value = zipmap(local.hostnames, local.instance_ids)
 }
