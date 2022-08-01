3e303725e1baea0e698ef43f2388512ac5a9ca81
Jakub Sokołowski <jakub@status.im>
2022-08-01 12:38:04 +0200

    drop SSH key for artur@status.im
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -90,7 +90,9 @@ variable "ssh_keys" {
   type        = list(string)
 
   # get a list of all available keys: `hcloud ssh-key list`
-  default     = ["jakub@status.im", "artur@status.im"]
+  default     = [
+    "jakub@status.im",
+  ]
 }
 
 /* FIREWALL --------------------------------------*/
